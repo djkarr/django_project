98fe50ea6ae73234b2119eb0a3d4c9ff0ae75b9e
Dan <dan.j.karr@gmail.com>
2022-01-10 17:58:12 -0700

    Revert "Removed key."
    This reverts commit 80b53e7671789415b59351d01bf7929145d19b5c.

--- daniels_django_laboratory/settings.py
+++ daniels_django_laboratory/settings.py
@@ -19,6 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'v)c%s83(*ro1o3)zdcv6h*e^2p^d(%dw#qx%48wrh(s)d8v4$('
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 ALLOWED_HOSTS = ["djkarr.pythonanywhere.com","localhost"]
 
 
